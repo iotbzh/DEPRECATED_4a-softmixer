793e4181cdcb5c5b3a0d3d77ea09df822dbe71cc
Jonathan Aillet <jonathan.aillet@iot.bzh>
2018-06-10 03:50:03 +0200

    Change afb binding version
    Change afb binding version to avoid compilation warnings.
    
    Change-Id: Ia8c8d8b5da04fba397a8e1e0879c433607e237d9
    Signed-off-by: Jonathan Aillet <jonathan.aillet@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -135,7 +135,7 @@ add_definitions(-DCONTROL_PLUGIN_PATH="${CMAKE_BINARY_DIR}/package/lib/plugins:$
 add_definitions(-DCONTROL_CONFIG_PATH="${CMAKE_SOURCE_DIR}/conf.d/project/etc:${CMAKE_BINARY_DIR}/package/etc:${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/etc")
 add_definitions(-DCONTROL_LUA_PATH="${CMAKE_SOURCE_DIR}/conf.d/project/lua.d:${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}/var")
 add_definitions(-DCTL_PLUGIN_MAGIC=987456123)
-add_definitions(-DUSE_API_DYN=1 -DAFB_BINDING_VERSION=dyn)
+add_definitions(-DUSE_API_DYN=1 -DAFB_BINDING_VERSION=0)
 
 
 # (BUG!!!) as PKG_CONFIG_PATH does not work [should be an env variable]
